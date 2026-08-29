ed7c57ec23544158299acc01516c7fb4cb91508f
Polaris Release <release@plrs.sh>
2026-08-29 10:21:41 +0000

    polaris 0.1.1

--- Formula/polaris.rb
+++ Formula/polaris.rb
@@ -1,25 +1,25 @@
 class Polaris < Formula
   desc "Continue Polaris coding Sessions on this machine"
   homepage "https://plrs.sh"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/plrs-sh/polaris-cli/releases/download/v0.1.0/polaris_darwin_arm64.tar.gz"
-      sha256 "9b61065559816ed111380efe9c057f73bdd315df6f890d37d43d4b488aa248fc"
+      url "https://github.com/plrs-sh/polaris-cli/releases/download/v0.1.1/polaris_darwin_arm64.tar.gz"
+      sha256 "27e66aed98580f7a4c582387825305511a6c0302f1ee2299c58abea1f79f271b"
     else
-      url "https://github.com/plrs-sh/polaris-cli/releases/download/v0.1.0/polaris_darwin_amd64.tar.gz"
-      sha256 "eadf663b034bc0df46aaec1f299d2cc6e63e22970a6ce6f2844b03fe82336da2"
+      url "https://github.com/plrs-sh/polaris-cli/releases/download/v0.1.1/polaris_darwin_amd64.tar.gz"
+      sha256 "921e304bf313a3945771b09236d2425930f0a47bdbded7057067bc85417ca0ae"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/plrs-sh/polaris-cli/releases/download/v0.1.0/polaris_linux_arm64.tar.gz"
-      sha256 "236a80e9fa9c05a92a14fa44a8ee5012b20811b34290b24516d9ba62b459a525"
+      url "https://github.com/plrs-sh/polaris-cli/releases/download/v0.1.1/polaris_linux_arm64.tar.gz"
+      sha256 "52df416e48d0ab7771f3c0be9fe54d36080b5423aec4945403ad2c3ce4a631bf"
     else
-      url "https://github.com/plrs-sh/polaris-cli/releases/download/v0.1.0/polaris_linux_amd64.tar.gz"
-      sha256 "63162c75f7cb18c5a64420786e16a2413dad3b6607036348cb00919308a77826"
+      url "https://github.com/plrs-sh/polaris-cli/releases/download/v0.1.1/polaris_linux_amd64.tar.gz"
+      sha256 "7b3c10a076cda8918b04c04c53c05e8a44e80bcf9cf698b98b5a664a91f29733"
     end
   end
 
@@ -28,6 +28,6 @@ class Polaris < Formula
   end
 
   test do
-    assert_match "polaris 0.1.0", shell_output("#{bin}/polaris version")
+    assert_match "polaris 0.1.1", shell_output("#{bin}/polaris version")
   end
 end
